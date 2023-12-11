b0978c3bd033c9f57658c74e92f6a9afeb2c04a7
Hellicat <jhccop@naver.com>
2023-12-12 00:26:10 +0900

    tarot api json data v0.1

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,26 @@ const app = express();
 app.use("/static", express.static("/public"));
 
 app.get("/card/major", (req, res) => {
-  const cardPath = path.join(__dirname, "./cards/major_card.json");
-  res.json(require(cardPath));
+  const majorCardPath = path.join(__dirname, "./cards/major_card.json");
+  res.json(require(majorCardPath));
 });
-
+app.get("/card/minor/cups", (req, res) => {
+  const cupsCardPath = path.join(__dirname, "./cards/minor_cups.json");
+  res.json(require(cupsCardPath));
+});
+app.get("/card/minor/sword", (req, res) => {
+  const swordCardPath = path.join(__dirname, "./cards/minor_sword.json");
+  res.json(require(swordCardPath));
+});
+app.get("/card/minor/wand", (req, res) => {
+  const wandCardPath = path.join(__dirname, "./cards/minor_wand.json");
+  res.json(require(wandCardPath));
+});
+app.get("/card/minor/pentacle", (req, res) => {
+  const pentacleCardPath = path.join(__dirname, "./cards/minor_pentacle.json");
+  res.json(require(pentacleCardPath));
+});
 const serverListen = () => {
-  console.log("Server is Connecting ✅");
+  console.log("Server is Connecting  http://localhost:3000 ✅");
 };
 app.listen(3000, serverListen);
