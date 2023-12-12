2134104a59d7cdf5d6d46152db4af2863a9ca52b
Hellicat <jhccop@naver.com>
2023-12-12 13:51:51 +0900

    edit json path

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import {readFileSync} from "fs";
 const PORT = 8080;
 const app = express();
+const __dirname = path.resolve();
+
 app.use(cors());
 
 app.use("/static", express.static("/public"));
@@ -12,23 +15,31 @@ app.get("/", (req, res) => {
 });
 app.get("/card/major", (req, res) => {
   const majorCardPath = path.join(__dirname, "./src/cards/major_card.json");
-  res.json(require(majorCardPath));
+  const jsonData = readFileSync(majorCardPath, "utf-8");
+  res.json(JSON.parse(jsonData));
 });
 app.get("/card/minor/cups", (req, res) => {
   const cupsCardPath = path.join(__dirname, "./src/cards/minor_cups.json");
-  res.json(require(cupsCardPath));
+  const jsonData = readFileSync(cupsCardPath, "utf-8");
+  res.json(JSON.parse(jsonData));
 });
 app.get("/card/minor/sword", (req, res) => {
   const swordCardPath = path.join(__dirname, "./src/cards/minor_sword.json");
-  res.json(require(swordCardPath));
+  const jsonData = readFileSync(swordCardPath, "utf-8");
+  res.json(JSON.parse(jsonData));
 });
 app.get("/card/minor/wand", (req, res) => {
   const wandCardPath = path.join(__dirname, "./src/cards/minor_wand.json");
-  res.json(require(wandCardPath));
+  const jsonData = readFileSync(wandCardPath, "utf-8");
+  res.json(JSON.parse(jsonData));
 });
 app.get("/card/minor/pentacle", (req, res) => {
-  const pentacleCardPath = path.join(__dirname, "./cards/minor_pentacle.json");
-  res.json(require(pentacleCardPath));
+  const pentacleCardPath = path.join(
+    __dirname,
+    "./src/cards/minor_pentacle.json"
+  );
+  const jsonData = readFileSync(pentacleCardPath, "utf-8");
+  res.json(JSON.parse(jsonData));
 });
 const serverListen = () => {
   console.log("Server is Connecting  http://localhost:8080 ✅");
